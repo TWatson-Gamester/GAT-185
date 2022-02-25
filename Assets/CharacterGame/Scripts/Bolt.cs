62f7be3b6ffb71cd7166737f37610aa8e8bd3db3
TWatson-Gamester <twatson@student.neumont.edu>
2022-02-24 20:02:58 -0700

    Adding of Particle Systems

---
+++ Assets/CharacterGame/Scripts/Bolt.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour
+{
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] Transform start;
+    [SerializeField] Transform end;
+    [SerializeField] int segments;
+    [SerializeField] float radius;
+    void Update()
+    {
+        Vector3[] positions = new Vector3[segments + 1];
+        Vector3 direction = (end.position - start.position).normalized;//< get direction vector from end to start >;
+        float length = segments;  //< get length of each segment>;
+
+        positions[0] = start.position;
+        positions[segments] = end.position;
+        for (int i = 1; i < segments; i++)
+        {
+            positions[i] = start.position + (direction.normalized * length * i);//< normalized direction* length * ???>;
+            positions[i] = positions[i] + Random.insideUnitSphere * radius;//< Random inside unit sphere* radius>;
+        }
+        lineRenderer.positionCount = positions.Length;
+        lineRenderer.SetPositions(positions);
+    }
+}
